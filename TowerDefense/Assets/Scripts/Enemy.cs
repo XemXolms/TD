f72ec2a0a6a08f9425751c5ede52353fa5fc1163
DESKTOP-ELLFJAI\cvan7 <cvan7060@gmail.com>
2020-08-30 13:39:12 +0300

    Turrets, waveSpawner, building effect, death effect, shop, UI, lifes

---
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    public int value = 50;
+    public float speed = 10f;
+    public GameObject deathEffect;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start ()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update ()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+
+        void GetNextWaypoint()
+        {
+            if ( waypointIndex >= Waypoints.points.Length - 1 ) 
+            {
+                EndPath();
+                return;
+            }
+
+            waypointIndex++;
+            target = Waypoints.points[waypointIndex];
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        PlayerStats.Money += value;
+
+        GameObject effect = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+
+        Destroy(gameObject);
+    }
+
+    void EndPath()
+    {
+        PlayerStats.Lives--;
+        Destroy(gameObject);
+    }
+}
---
+++ TowerDefense/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerStats: MonoBehaviour
+{
+	public static int Lives;
+	public int startLives = 20;
+
+	public static int Money;
+	public int startMoney = 400;
+
+	void Start()
+	{
+		Money = startMoney;
+		Lives = startLives;
+	}
+}
