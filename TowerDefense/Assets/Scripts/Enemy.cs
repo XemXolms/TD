debc03286fc6d8dec0cf4c77f5622ecac88da41e
DESKTOP-OLN0LCT\Gagarin <cvan7060@gmail.com>
2020-09-20 10:48:45 +0300

    Fading, wave Spawner, level Select

--- TowerDefense/Assets/Scripts/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -44,6 +44,9 @@ public class Enemy : MonoBehaviour
 		PlayerStats.Money += worth;
 		GameObject effect = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
 		Destroy(effect, 5f);
+
+		WaveSpawner.EnemiesAlive--;
+
 		Destroy(gameObject);
 	}
 }
